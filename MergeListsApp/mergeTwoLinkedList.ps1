c0230913600cf071b88a6edeae91c89723a95792
ochampo <danocampo@meta.com>
2025-11-24 15:36:31 -0800

    linkList merge two sorted list in powrshell, working but not tested with a lot of test cases.

--- MergeListsApp/mergeTwoLinkedList.ps1
+++ MergeListsApp/mergeTwoLinkedList.ps1
@@ -14,14 +14,58 @@ ListNode($value=0 )
 
 class Solution {
 
-  $dummy =  [ListNode]::new()
-  $current = $dummy
-
-  while($list1 -ne $null -and $list2 -ne $null )
+static [ListNode] mergeTwoList([ListNode] $list1, [ListNode] $list2)
   {
-    
-  }
+     $dummy =  [ListNode]::new(0)
+     $current = $dummy
+     while($null -ne $list1 -and $null -ne $list2)
+     {
+      if($list1.value -lt $list2.value)
+      {
+        $current.next = $list1;
+        $list1 = $list1.next
+      }
+      else
+      {
+        $current.next = $list2;
+        $list2 = $list2.next
+      }
+      $current = $current.next
+
+      
+     }
+
+     $current.next = ($null -ne $list1) ? $list1 : $list2
+     
+     return $dummy.next
+
+  }  
+
  
-
-
 }
+
+
+function convertto-LinkedList {
+    param (
+        [array]$list
+    )
+   [ListNode] $dummy =  [ListNode]::new(0)
+   [ListNode] $current = $dummy
+    foreach($item in $list)
+    {
+      $current.next = [ListNode]::new($item)
+      $current = $current.next
+    }
+    return $dummy.next
+}
+
+# Example usage:
+$array1 = @( 1, 2, 3)
+$array2 = @(1, 2, 3)
+
+$list1 = convertto-LinkedList $array1
+$list2 = convertto-LinkedList $array2
+
+
+
+ [Solution]::mergeTwoList($list1, $list2)
